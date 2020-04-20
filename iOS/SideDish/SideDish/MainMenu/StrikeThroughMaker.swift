13acc46b2997c805ff35c62e64a116efc5e93c51
GangWoon <gangwoon2018@gmail.com>
2020-04-20 16:54:44 +0900

    [#5] feat: StrikeThrough 객체 생성
    할인 가격을 표시하는 label은  StrikeThrough로 표시해야하기 떄문에 이 책임을 담당할 클래스를 만들었습니다.

---
+++ iOS/SideDish/SideDish/MainMenu/StrikeThroughMaker.swift
@@ -0,0 +1,18 @@
+//
+//  StrikeThroughMaker.swift
+//  SideDish
+//
+//  Created by Cloud on 2020/04/20.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+struct StrikeThroughMaker {
+    static func apply(_ input: String) -> NSMutableAttributedString {
+        let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: input)
+        attributedString.addAttributes([NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue]
+            , range: NSMakeRange(0, input.count))
+        return attributedString
+    }
+}
