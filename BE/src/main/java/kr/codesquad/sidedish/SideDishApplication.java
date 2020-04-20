b90e8263908bd8820717b3259d65deee78b7b7fb
IdionKim <ksundong@gmail.com>
2020-04-20 20:47:11 +0900

    [#9] feat: Spring Project 시작
    스프링 프로젝트를 초기화 합니다.
    - 스프링 프로젝트 요구사항대로 initialize
    - gitignore 설정
    - application properties 분리
    - Custom Exception 정의
    - Global Exception Handler 정의
    - Jackson Config 설정
    - Jasypt Config 설정
    - Cors Config 설정
    - Swagger Config 설정
    - temp.md를 README.md 로 변경
    
    Close #9 Issue.

---
+++ BE/src/main/java/kr/codesquad/sidedish/SideDishApplication.java
@@ -0,0 +1,13 @@
+package kr.codesquad.sidedish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideDishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideDishApplication.class, args);
+	}
+
+}
