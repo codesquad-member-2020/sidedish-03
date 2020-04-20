c8ff5843779805818fa528620600ef1b9a4e59fe
GangWoon <gangwoon2018@gmail.com>
2020-04-20 21:49:17 +0900

    [#5]  Custom Section Header 생성
    디자인과 동일한 섹현헤더를 생성했습니다.
    의문점은 센터X를 주고 리딩값을 주면 기기마다 width가 다르기때문에 레이블이 꺠지는현상이 발생할텐데 이런 부분은 어떻게 처리하면 좋을까요??
    스토리보드로 작성하면  Greater Than or Equal을 설정해줄수 있지만 코드로도 가능할까요??

--- iOS/SideDish/SideDish/MainMenu/TableView/MainMenuTableViewSectionHeader.swift
+++ iOS/SideDish/SideDish/MainMenu/TableView/MainMenuTableViewSectionHeader.swift
@@ -7,7 +7,89 @@
 //
 
 import UIKit
+import SnapKit
+
+extension UIFont {
+    static var categorySize: CGFloat = 12
+    static var boldTitleSize: CGFloat = 16
+}
+
+extension CALayer {
+    static var categoryWidth: CGFloat = 2
+}
 
 class MainMenuTableViewSectionHeader: UITableViewHeaderFooterView {
     
+    // MARK: - IBOutlets
+    private var colorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    private var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .systemGray
+        label.font = UIFont.systemFont(ofSize: UIFont.categorySize)
+        label.layer.borderWidth = CALayer.categoryWidth
+        label.layer.borderColor = UIColor.systemGray.cgColor
+        return label
+    }()
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: UIFont.boldTitleSize)
+        return label
+    }()
+    
+    // MARK: - Properties
+    static var height: CGFloat = 50
+    
+    // MARK: - Lifecyles
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    // MARK: - Methods
+    private func configure() {
+        configureBackgroundView()
+        configureCategoryLabel()
+        configureTitleLabel()
+    }
+    
+    private func configureBackgroundView() {
+        addSubview(colorView)
+        colorView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    private func configureCategoryLabel() {
+        colorView.addSubview(categoryLabel)
+        categoryLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(4)
+            make.leading.equalToSuperview().inset(160)
+        }
+    }
+    
+    private func configureTitleLabel() {
+        colorView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(categoryLabel.snp.bottom).offset(4)
+            make.centerX.equalToSuperview()
+            make.leading.equalToSuperview().inset(40)
+        }
+    }
+    
+    func update(category: String, title: String) {
+        categoryLabel.text = category
+        titleLabel.text = title
+    }
 }
