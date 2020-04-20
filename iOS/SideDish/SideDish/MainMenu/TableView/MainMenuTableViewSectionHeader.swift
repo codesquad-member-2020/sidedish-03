7f226ffd673fd3f85452191fc8f7dd6d1a6e5a56
GangWoon <gangwoon2018@gmail.com>
2020-04-20 17:18:49 +0900

    [#5] style: Section Header 클래스 생성

---
+++ iOS/SideDish/SideDish/MainMenu/TableView/MainMenuTableViewSectionHeader.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenuTableViewSectionHeader.swift
+//  SideDish
+//
+//  Created by Cloud on 2020/04/20.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewSectionHeader: UITableViewHeaderFooterView {
+    
+}
