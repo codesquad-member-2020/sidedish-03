bc6afa445593cc2fda6e16cfc8fb04c605b389ec
GangWoon <gangwoon2018@gmail.com>
2020-04-20 16:15:13 +0900

    [#1] style: Custom TableView Cell 생성
    기본적인 틀을 잡았습니다.

---
+++ iOS/SideDish/SideDish/MainMenu/MainMenuTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenuTableViewCell.swift
+//  SideDish
+//
+//  Created by Cloud on 2020/04/20.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewCell: UITableViewCell {
+    
+}
