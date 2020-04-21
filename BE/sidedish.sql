9c2950bae455c66bb2f8df10aa6726e65cc8b288
IdionKim <ksundong@gmail.com>
2020-04-21 15:07:25 +0900

    [#22] refactor: DB구조 변경
    - image 관련 테이블을 하나로 합쳤습니다.
    - 필요없는 field인 detail_hash를 제거합니다.
    - User 테이블을 추가합니다.

--- BE/sidedish.sql
+++ BE/sidedish.sql
@@ -1,44 +1,46 @@
-ALTER TABLE `sidedish`.`side_dish`
+ALTER TABLE `side_dish`
     DROP FOREIGN KEY `fk_side_dish_image_1`;
-ALTER TABLE `sidedish`.`side_dish`
+ALTER TABLE `side_dish`
     DROP FOREIGN KEY `fk_side_dish_image_2`;
-ALTER TABLE `sidedish`.`detail_section_image`
+ALTER TABLE `detail_section_image`
     DROP FOREIGN KEY `fk_detail_section_image_side_dish_1`;
-ALTER TABLE `sidedish`.`thumb_image`
+ALTER TABLE `thumb_image`
     DROP FOREIGN KEY `fk_thumb_image_side_dish_1`;
-ALTER TABLE `sidedish`.`category_side_dish`
+ALTER TABLE `category_side_dish`
     DROP FOREIGN KEY `fk_category_side_dish_side_dish_1`;
-ALTER TABLE `sidedish`.`category_side_dish`
+ALTER TABLE `category_side_dish`
     DROP FOREIGN KEY `fk_category_side_dish_category_1`;
-ALTER TABLE `sidedish`.`delivery_type_side_dish`
+ALTER TABLE `delivery_type_side_dish`
     DROP FOREIGN KEY `fk_delivery_type_side_dish_delivery_type_1`;
-ALTER TABLE `sidedish`.`delivery_type_side_dish`
+ALTER TABLE `delivery_type_side_dish`
     DROP FOREIGN KEY `fk_delivery_type_side_dish_side_dish_1`;
-ALTER TABLE `sidedish`.`badge_side_dish`
+ALTER TABLE `badge_side_dish`
     DROP FOREIGN KEY `fk_badge_side_dish_badge_1`;
-ALTER TABLE `sidedish`.`badge_side_dish`
+ALTER TABLE `badge_side_dish`
     DROP FOREIGN KEY `fk_badge_side_dish_side_dish_1`;
-ALTER TABLE `sidedish`.`side_dish`
+ALTER TABLE `side_dish`
     DROP FOREIGN KEY `fk_side_dish_food_type_1`;
+ALTER TABLE `image`
+    DROP FOREIGN KEY `fk_image_side_dish_1`;
 
-DROP TABLE IF EXISTS `sidedish`.`side_dish`;
-DROP TABLE IF EXISTS `sidedish`.`image`;
-DROP TABLE IF EXISTS `sidedish`.`detail_section_image`;
-DROP TABLE IF EXISTS `sidedish`.`thumb_image`;
-DROP TABLE IF EXISTS `sidedish`.`category`;
-DROP TABLE IF EXISTS `sidedish`.`category_side_dish`;
-DROP TABLE IF EXISTS `sidedish`.`food_type`;
-DROP TABLE IF EXISTS `sidedish`.`delivery_type`;
-DROP TABLE IF EXISTS `sidedish`.`badge`;
-DROP TABLE IF EXISTS `sidedish`.`delivery_type_side_dish`;
-DROP TABLE IF EXISTS `sidedish`.`badge_side_dish`;
+DROP TABLE `side_dish`;
+DROP TABLE `image`;
+DROP TABLE `detail_section_image`;
+DROP TABLE `thumb_image`;
+DROP TABLE `category`;
+DROP TABLE `category_side_dish`;
+DROP TABLE `food_type`;
+DROP TABLE `delivery_type`;
+DROP TABLE `badge`;
+DROP TABLE `delivery_type_side_dish`;
+DROP TABLE `badge_side_dish`;
+DROP TABLE `user`;
 
-CREATE TABLE IF NOT EXISTS `sidedish`.`side_dish`
+CREATE TABLE `side_dish`
 (
     `id`            BIGINT(11)    NOT NULL AUTO_INCREMENT,
-    `detail_hash`   VARCHAR(255)  NULL,
-    `main_image`    BIGINT(255)   NULL,
-    `top_image`     BIGINT(255)   NULL,
+    `main_image`    BIGINT(11)    NULL,
+    `top_image`     BIGINT(11)    NULL,
     `alt`           VARCHAR(255)  NULL,
     `title`         VARCHAR(255)  NULL,
     `description`   VARCHAR(1000) NULL,
@@ -50,21 +52,16 @@ CREATE TABLE IF NOT EXISTS `sidedish`.`side_dish`
     `food_type`     BIGINT(11)    NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`image`
+CREATE TABLE `image`
 (
-    `id`  BIGINT(11)   NOT NULL AUTO_INCREMENT,
-    `url` VARCHAR(255) NULL,
+    `id`                         BIGINT(11)   NOT NULL AUTO_INCREMENT,
+    `url`                        VARCHAR(255) NULL,
+    `side_dish`                  BIGINT(11)   NULL,
+    `detail_section_image_order` INT(11)      NULL,
+    `thumb_image_order`          INT(11)      NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`detail_section_image`
-(
-    `id`            BIGINT(11)   NOT NULL AUTO_INCREMENT,
-    `url`           VARCHAR(255) NULL,
-    `side_dish`     BIGINT(11)   NULL,
-    `side_dish_key` INT(11)      NULL,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE IF NOT EXISTS `sidedish`.`thumb_image`
+CREATE TABLE `detail_section_image`
 (
     `id`            BIGINT(11)   NOT NULL AUTO_INCREMENT,
     `url`           VARCHAR(255) NULL,
@@ -72,72 +69,89 @@ CREATE TABLE IF NOT EXISTS `sidedish`.`thumb_image`
     `side_dish_key` INT(11)      NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`category`
+CREATE TABLE `thumb_image`
+(
+    `id`            BIGINT(11)   NOT NULL AUTO_INCREMENT,
+    `url`           VARCHAR(255) NULL,
+    `side_dish`     BIGINT(11)   NULL,
+    `side_dish_key` INT(11)      NULL,
+    PRIMARY KEY (`id`)
+);
+CREATE TABLE `category`
 (
     `id`   BIGINT(11)   NOT NULL,
     `name` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`category_side_dish`
+CREATE TABLE `category_side_dish`
 (
     `id`        BIGINT(11) NOT NULL AUTO_INCREMENT,
     `category`  BIGINT(11) NULL,
     `side_dish` BIGINT(11) NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`food_type`
+CREATE TABLE `food_type`
 (
     `id`   BIGINT(11)   NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`delivery_type`
+CREATE TABLE `delivery_type`
 (
     `id`   BIGINT(11)   NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`badge`
+CREATE TABLE `badge`
 (
     `id`   BIGINT(11)   NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`delivery_type_side_dish`
+CREATE TABLE `delivery_type_side_dish`
 (
     `id`            BIGINT(11) NOT NULL,
     `delivery_type` BIGINT(11) NULL,
     `side_dish`     BIGINT(11) NULL,
     PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `sidedish`.`badge_side_dish`
+CREATE TABLE `badge_side_dish`
 (
     `id`        BIGINT(11) NOT NULL AUTO_INCREMENT,
     `badge`     BIGINT(11) NULL,
     `side_dish` BIGINT(11) NULL,
     PRIMARY KEY (`id`)
 );
+CREATE TABLE `user`
+(
+    `id`           BIGINT(11)   NOT NULL,
+    `user_name`    VARCHAR(255) NULL,
+    `access_token` VARCHAR(255) NULL,
+    PRIMARY KEY (`id`)
+);
 
-ALTER TABLE `sidedish`.`side_dish`
-    ADD CONSTRAINT `fk_side_dish_image_1` FOREIGN KEY (`main_image`) REFERENCES `sidedish`.`image` (`id`);
-ALTER TABLE `sidedish`.`side_dish`
-    ADD CONSTRAINT `fk_side_dish_image_2` FOREIGN KEY (`top_image`) REFERENCES `sidedish`.`image` (`id`);
-ALTER TABLE `sidedish`.`detail_section_image`
-    ADD CONSTRAINT `fk_detail_section_image_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `sidedish`.`side_dish` (`id`);
-ALTER TABLE `sidedish`.`thumb_image`
-    ADD CONSTRAINT `fk_thumb_image_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `sidedish`.`side_dish` (`id`);
-ALTER TABLE `sidedish`.`category_side_dish`
-    ADD CONSTRAINT `fk_category_side_dish_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `sidedish`.`side_dish` (`id`);
-ALTER TABLE `sidedish`.`category_side_dish`
-    ADD CONSTRAINT `fk_category_side_dish_category_1` FOREIGN KEY (`category`) REFERENCES `sidedish`.`category` (`id`);
-ALTER TABLE `sidedish`.`delivery_type_side_dish`
-    ADD CONSTRAINT `fk_delivery_type_side_dish_delivery_type_1` FOREIGN KEY (`delivery_type`) REFERENCES `sidedish`.`delivery_type` (`id`);
-ALTER TABLE `sidedish`.`delivery_type_side_dish`
-    ADD CONSTRAINT `fk_delivery_type_side_dish_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `sidedish`.`side_dish` (`id`);
-ALTER TABLE `sidedish`.`badge_side_dish`
-    ADD CONSTRAINT `fk_badge_side_dish_badge_1` FOREIGN KEY (`badge`) REFERENCES `sidedish`.`badge` (`id`);
-ALTER TABLE `sidedish`.`badge_side_dish`
-    ADD CONSTRAINT `fk_badge_side_dish_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `sidedish`.`side_dish` (`id`);
-ALTER TABLE `sidedish`.`side_dish`
-    ADD CONSTRAINT `fk_side_dish_food_type_1` FOREIGN KEY (`food_type`) REFERENCES `sidedish`.`food_type` (`id`);
+ALTER TABLE `side_dish`
+    ADD CONSTRAINT `fk_side_dish_image_1` FOREIGN KEY (`main_image`) REFERENCES `image` (`id`);
+ALTER TABLE `side_dish`
+    ADD CONSTRAINT `fk_side_dish_image_2` FOREIGN KEY (`top_image`) REFERENCES `image` (`id`);
+ALTER TABLE `detail_section_image`
+    ADD CONSTRAINT `fk_detail_section_image_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `side_dish` (`id`);
+ALTER TABLE `thumb_image`
+    ADD CONSTRAINT `fk_thumb_image_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `side_dish` (`id`);
+ALTER TABLE `category_side_dish`
+    ADD CONSTRAINT `fk_category_side_dish_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `side_dish` (`id`);
+ALTER TABLE `category_side_dish`
+    ADD CONSTRAINT `fk_category_side_dish_category_1` FOREIGN KEY (`category`) REFERENCES `category` (`id`);
+ALTER TABLE `delivery_type_side_dish`
+    ADD CONSTRAINT `fk_delivery_type_side_dish_delivery_type_1` FOREIGN KEY (`delivery_type`) REFERENCES `delivery_type` (`id`);
+ALTER TABLE `delivery_type_side_dish`
+    ADD CONSTRAINT `fk_delivery_type_side_dish_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `side_dish` (`id`);
+ALTER TABLE `badge_side_dish`
+    ADD CONSTRAINT `fk_badge_side_dish_badge_1` FOREIGN KEY (`badge`) REFERENCES `badge` (`id`);
+ALTER TABLE `badge_side_dish`
+    ADD CONSTRAINT `fk_badge_side_dish_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `side_dish` (`id`);
+ALTER TABLE `side_dish`
+    ADD CONSTRAINT `fk_side_dish_food_type_1` FOREIGN KEY (`food_type`) REFERENCES `food_type` (`id`);
+ALTER TABLE `image`
+    ADD CONSTRAINT `fk_image_side_dish_1` FOREIGN KEY (`side_dish`) REFERENCES `side_dish` (`id`);
 
