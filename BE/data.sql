5837c765a45d507225ca356bbc25b6ba7c67aabd
blossun <dnsngu@gmail.com>
2020-04-21 19:52:28 +0900

    [#24] feat: DB data 추가
    * food_type, category, delivery_type, badge 데이터 추가

---
+++ BE/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO food_type(name) VALUES ('든든한반찬');
+INSERT INTO food_type(name) VALUES ('국물요리');
+INSERT INTO food_type(name) VALUES ('밑반찬');
+
+INSERT INTO category(id, name) VALUES ('17011200', '할인특가세트상품');
+INSERT INTO category(id, name) VALUES ('17011000', '풍성한고기반찬');
+INSERT INTO category(id, name) VALUES ('17010200', '편리한반찬세트');
+INSERT INTO category(id, name) VALUES ('17010330', '간편한 덮밥요리');
+INSERT INTO category(id, name) VALUES ('17011400', '우리아이영양반찬');
+
+INSERT INTO delivery_type(id, name) VALUES (1, '새벽배송');
+INSERT INTO delivery_type(id, name) VALUES (2, '전국택배');
+
+INSERT INTO badge(id, name) VALUES (1, '론칭특가');
+INSERT INTO badge(id, name) VALUES (2, '이벤트특');
+INSERT INTO badge(id, name) VALUES (3, '베스트');
