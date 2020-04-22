f08978376e176245f2d33bb559a6c5a98f8aeb91
blossun <dnsngu@gmail.com>
2020-04-22 18:29:51 +0900

    [#21] refactor: 불필요한 코드 제거
    - client_secret 값을 환경변수로 등록하여 불러오도록 수정

--- BE/src/main/java/kr/codesquad/sidedish/common/login/LoginService.java
+++ BE/src/main/java/kr/codesquad/sidedish/common/login/LoginService.java
@@ -1,7 +1,5 @@
 package kr.codesquad.sidedish.common.login;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
@@ -17,28 +15,26 @@ import java.util.Map;
 @Service
 public class LoginService {
     private static final Logger log = LoggerFactory.getLogger(LoginService.class);
-    String url = "https://github.com/login/oauth/access_token";
-    String client_id = "bed01aae4e0ea3bebf24";
-    String client_secret = "452595b0393eef42655db16938b856735d978c32";
+    private String url = "https://github.com/login/oauth/access_token";
+    private String clientId = System.getenv("GITHUB_CLIENT_ID");
+    private String clientSecret = System.getenv("GITHUB_CLIENT_SECRET");
 
-    public GithubToken getAccessToken(String code) throws JsonProcessingException {
+    public GithubToken getAccessToken(String code) {
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        Map header = new HashMap<String, String>();
+        Map<String, String> header = new HashMap<>();
         header.put("Accept", "application/json");
         headers.setAll(header);
 
         MultiValueMap<String, String> requestPayloads = new LinkedMultiValueMap<>();
-        Map requestPayload = new HashMap<String, String>();
-        requestPayload.put("client_id", client_id);
-        requestPayload.put("client_secret", client_secret);
+        Map<String, String> requestPayload = new HashMap<>();
+        requestPayload.put("client_id", clientId);
+        requestPayload.put("client_secret", clientSecret);
         requestPayload.put("code", code);
         requestPayloads.setAll(requestPayload);
 
         HttpEntity<?> request = new HttpEntity<>(requestPayloads, headers);
-        ResponseEntity<?> response = new RestTemplate().postForEntity(url, request, String.class);
-        String responseBody = response.getBody().toString();
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(responseBody, GithubToken.class);
+        ResponseEntity<?> response = new RestTemplate().postForEntity(url, request, GithubToken.class);
+        return (GithubToken) response.getBody();
     }
 
 }
