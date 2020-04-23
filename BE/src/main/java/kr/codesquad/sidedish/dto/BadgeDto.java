14497937170bbb08e77d9ce74d350e31fb9d6318
blossun <dnsngu@gmail.com>
2020-04-23 16:29:57 +0900

    [#46] feat: Dto 생성
    - CategoryDto, BadgeDto, DishDto, DishOverviewDto를 생성

---
+++ BE/src/main/java/kr/codesquad/sidedish/dto/BadgeDto.java
@@ -0,0 +1,22 @@
+package kr.codesquad.sidedish.dto;
+
+public class BadgeDto {
+    private String name;
+    private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
