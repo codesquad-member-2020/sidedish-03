1ae95771d92c7f7a457b95e0811b64f78706456f
IdionKim <ksundong@gmail.com>
2020-04-23 21:27:24 +0900

    [#33] feat: NamedParameterJdbcTemplate가 좀 기능이 부실해서 상속해서 추가기능 구현
    Optional을 활용할 수 있도록 하였습니다.

---
+++ BE/src/main/java/kr/codesquad/sidedish/common/util/NamedParameterOptionalJdbcTemplate.java
@@ -0,0 +1,40 @@
+package kr.codesquad.sidedish.common.util;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class NamedParameterOptionalJdbcTemplate extends NamedParameterJdbcTemplate {
+    public NamedParameterOptionalJdbcTemplate(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    public <T> Optional<T> queryForOptionalObject(String sql, SqlParameterSource parameterSource, RowMapper<T> rowMapper) {
+        List<T> results = getJdbcOperations().query(getPreparedStatementCreator(sql, parameterSource), rowMapper);
+
+        if (results.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.ofNullable(results.iterator().next());
+        }
+    }
+
+    public <T> Optional<T> queryForOptionalObject(String sql, Map<String, ?> paramMap, RowMapper<T> rowMapper) {
+        return queryForOptionalObject(sql, new MapSqlParameterSource(paramMap), rowMapper);
+    }
+
+    public <T> Optional<T> queryForOptionalObject(String sql, SqlParameterSource parameterSource, Class<T> requiredType) {
+        return queryForOptionalObject(sql, parameterSource, new SingleColumnRowMapper<>(requiredType));
+    }
+
+    public <T> Optional<T> queryForOptionalObject(String sql, Map<String, ?> paramMap, Class<T> requiredType) {
+        return queryForOptionalObject(sql, paramMap, new SingleColumnRowMapper<>(requiredType));
+    }
+}
