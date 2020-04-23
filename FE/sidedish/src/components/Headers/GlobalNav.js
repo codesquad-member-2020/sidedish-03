9d6aebdbf5214c5d19aba4581361463ac39ce673
baekCode <sosaer88@gmail.com>
2020-04-23 14:18:23 +0900

    [#28] style: 코드 prettier 정리

--- FE/sidedish/src/components/Headers/GlobalNav.js
+++ FE/sidedish/src/components/Headers/GlobalNav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import useAsync from '../../utils/useAsync';
-import {MOCK_URL} from '../../constant/url';
+import { MOCK_URL } from '../../constant/url';
 import '../../style/gnb.scss';
 
 const GlobalNavStyled = styled.div`
@@ -11,13 +11,13 @@ const GlobalNavStyled = styled.div`
   z-index: 10;
 `;
 
-const getMenuList = async() => {
+const getMenuList = async () => {
   const response = await axios.get(`${MOCK_URL}data/menu.json`);
   return response.data;
-}
+};
 
 const GlobalNav = () => {
-  const state = useAsync(getMenuList)
+  const state = useAsync(getMenuList);
   const { loading, data: menuList, error } = state;
   if (loading) return <div>로딩중</div>;
   if (error) return <div>에러</div>;
