86066b80466d6b2bf8435fa27687bd62d9fc9837
baekCode <sosaer88@gmail.com>
2020-04-23 14:03:40 +0900

    [#28] feat: 상수처리된 URL, useAsync 사용

--- FE/sidedish/src/components/Headers/GlobalNav.js
+++ FE/sidedish/src/components/Headers/GlobalNav.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
-import useAsync from '../../utils/useAsync.js';
+import useAsync from '../../utils/useAsync';
+import {MOCK_URL} from '../../constant/url';
 import '../../style/gnb.scss';
 
 const GlobalNavStyled = styled.div`
@@ -10,13 +11,13 @@ const GlobalNavStyled = styled.div`
   z-index: 10;
 `;
 
-const getMenuList = async () => {
-  const response = await axios.get('http://localhost:3000/data/menu.json');
+const getMenuList = async() => {
+  const response = await axios.get(`${MOCK_URL}data/menu.json`);
   return response.data;
-};
+}
 
 const GlobalNav = () => {
-  const state  = useAsync(getMenuList, []);
+  const state = useAsync(getMenuList)
   const { loading, data: menuList, error } = state;
   if (loading) return <div>로딩중</div>;
   if (error) return <div>에러</div>;
