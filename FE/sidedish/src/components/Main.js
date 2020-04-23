28a3577940192d4621ce7f17cde85f2faaea4cd9
baekCode <sosaer88@gmail.com>
2020-04-23 14:20:40 +0900

    [#28] feat: Main component props url 전달

--- FE/sidedish/src/components/Main.js
+++ FE/sidedish/src/components/Main.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import Carousel from "./Carousel";
+
+const MainStyled = styled.div`
+  min-height: 600px;
+`;
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <MainStyled>
+      <div className='inner'>
+        <Carousel url="main"/>
+      </div>
+    </MainStyled>
+  );
 };
 
 export default Main;
