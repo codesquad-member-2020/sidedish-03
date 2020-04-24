850a25af85068ebdf8559dfa8ab2dc01869ebd1d
IdionKim <ksundong@gmail.com>
2020-04-24 18:46:26 +0900

    [#32] chore: War 배포를 위한 설정 추가

--- BE/src/main/java/kr/codesquad/sidedish/SideDishApplication.java
+++ BE/src/main/java/kr/codesquad/sidedish/SideDishApplication.java
@@ -2,9 +2,10 @@ package kr.codesquad.sidedish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SideDishApplication {
+public class SideDishApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SideDishApplication.class, args);
