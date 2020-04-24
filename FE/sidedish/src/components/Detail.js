1a36c933d1c75ba195f6c15f4c596dd8791e82b3
baekCode <sosaer88@gmail.com>
2020-04-24 13:14:13 +0900

    [#55] feat: Detail 썸네일 이미지 슬라이더 스켈레톤 구현

--- FE/sidedish/src/components/Detail.js
+++ FE/sidedish/src/components/Detail.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Mask = styled.div`
   display: flex;
@@ -13,27 +16,58 @@ const Mask = styled.div`
   background: #f7f9fa;
   z-index: 100;
   .detail-wrap {
+    overflow-y: auto;
     width: 800px;
+    height: 100%;
     background: #fff;
   }
 `;
 
 const Detail = props => {
   console.log(props.data.item.data);
-
+  const settings = {
+    customPaging: function(i) {
+      return (
+        <a>
+          <img src={`${props.data.item.data.thumb_images[i]}`} />
+        </a>
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
   return (
     <Mask className='mask'>
       <div className='detail-wrap'>
         <div className='detail-head'>
-          <div className='detail-thumb'></div>
+          <Slider {...settings} className='detail-thumb'>
+            {props.data.item.data.thumb_images.map((img, index) => (
+              <div className='thumb' key={index}>
+                <img src={img} />
+              </div>
+            ))}
+          </Slider>
           <div className='detail-info'>
             <div className='badge'></div>
             <h3 className='title'>{props.data.title}</h3>
             <p className='description'>{props.data.item.data.product_description}</p>
             <div className='table'>
-              <div><span>적립금</span><span>{props.data.item.data.point}</span></div>
-              <div><span>배송정보</span><span>{props.data.item.data.delivery_info}</span></div>
-              <div><span>배송비</span><span>{props.data.item.data.delivery_fee}</span></div>
+              <div>
+                <span>적립금</span>
+                <span>{props.data.item.data.point}</span>
+              </div>
+              <div>
+                <span>배송정보</span>
+                <span>{props.data.item.data.delivery_info}</span>
+              </div>
+              <div>
+                <span>배송비</span>
+                <span>{props.data.item.data.delivery_fee}</span>
+              </div>
             </div>
             <div className='quantity'></div>
             <div className='total'></div>
