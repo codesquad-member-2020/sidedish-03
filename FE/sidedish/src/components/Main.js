b389dbee0d2b34b4fe0c163da8a16151aa19d673
baekCode <sosaer88@gmail.com>
2020-04-24 12:33:43 +0900

    [#55] feat: Main component 에서 캐러셀 클릭시 props & useState

--- FE/sidedish/src/components/Main.js
+++ FE/sidedish/src/components/Main.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import Carousel from "./Carousel";
+import Carousel from './Carousel';
+import Detail from './Detail';
 
 const MainStyled = styled.div`
   min-height: 600px;
 `;
 
 const Main = () => {
+  const [ detail, setDetail] = useState(null);
+  const onClickDetailItem = (item,title) => {
+    setDetail({item,title});
+  };
   return (
     <MainStyled>
       <div className='inner'>
-        <Carousel url="main"/>
-        <Carousel url="soup"/>
-        <Carousel url="side"/>
+        <Carousel url='main' onClickHandler={onClickDetailItem} />
+        <Carousel url='soup' onClickHandler={onClickDetailItem} />
+        <Carousel url='side' onClickHandler={onClickDetailItem} />
       </div>
+      {!detail ?  '' : <Detail data={detail}/>}
     </MainStyled>
   );
 };
