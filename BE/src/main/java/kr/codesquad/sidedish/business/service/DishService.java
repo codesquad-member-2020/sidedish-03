a2d657c909625b674666cdf67c25537a42c7c370
blossun <dnsngu@gmail.com>
2020-04-24 10:53:55 +0900

    [#33] Autowire 오류 문제 해결
    - 타입이 2개라 Spring이 autowire를 해주지 못해서 이름을 지정하여 빈을 등록해줌

--- BE/src/main/java/kr/codesquad/sidedish/business/service/DishService.java
+++ BE/src/main/java/kr/codesquad/sidedish/business/service/DishService.java
@@ -4,6 +4,7 @@ import kr.codesquad.sidedish.business.dao.DishDao;
 import kr.codesquad.sidedish.business.dto.DishDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +13,7 @@ public class DishService {
 
     private final DishDao dishDao;
 
-    public DishService(DishDao dishDao) {
+    public DishService(@Qualifier("dishDaoSolar") DishDao dishDao) {
         this.dishDao = dishDao;
     }
 
