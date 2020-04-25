a2a4f245265aa98825bd5119d9cba330c5344fa7
IdionKim <ksundong@gmail.com>
2020-04-25 23:42:12 +0900

    [#69] refactor: DishOverviewDto 리팩토링
    일부 setter를 제거하고, Dto 자체를 어느정도 Immutable하게 만듭니다.
    Builder 패턴을 적용해보았습니다.

--- BE/src/main/java/kr/codesquad/sidedish/business/dto/DishOverviewDto.java
+++ BE/src/main/java/kr/codesquad/sidedish/business/dto/DishOverviewDto.java
@@ -4,38 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DishOverviewDto {
-    private Long id;
-    private String mainImage;
-    private String alt;
-    private String title;
-    private String description;
-    private String normalPrice;
-    private String salePrice;
+    private final Long id;
+    private final String mainImage;
+    private final String alt;
+    private final String title;
+    private final String description;
+    private final String normalPrice;
+    private final String salePrice;
     private List<String> deliveryTypes = new ArrayList<>();
     private List<BadgeDto> badges = new ArrayList<>();
 
-    public Long getId() {
-        return id;
+    private DishOverviewDto(Builder builder) {
+        this.id = builder.id;
+        this.mainImage = builder.mainImage;
+        this.alt = builder.alt;
+        this.title = builder.title;
+        this.description = builder.description;
+        this.normalPrice = builder.normalPrice;
+        this.salePrice = builder.salePrice;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long getId() {
+        return id;
     }
 
     public String getMainImage() {
         return mainImage;
     }
 
-    public void setMainImage(String mainImage) {
-        this.mainImage = mainImage;
-    }
-
     public String getAlt() {
         return alt;
     }
 
-    public void setAlt(String alt) {
-        this.alt = alt;
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getNormalPrice() {
+        return normalPrice;
+    }
+
+    public String getSalePrice() {
+        return salePrice;
     }
 
     public List<String> getDeliveryTypes() {
@@ -44,38 +58,6 @@ public class DishOverviewDto {
 
     public void setDeliveryTypes(List<String> deliveryTypes) {
         this.deliveryTypes = deliveryTypes;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getNormalPrice() {
-        return normalPrice;
-    }
-
-    public void setNormalPrice(String normalPrice) {
-        this.normalPrice = normalPrice;
-    }
-
-    public String getSalePrice() {
-        return salePrice;
-    }
-
-    public void setSalePrice(String salePrice) {
-        this.salePrice = salePrice;
     }
 
     public List<BadgeDto> getBadges() {
@@ -89,5 +71,53 @@ public class DishOverviewDto {
     @Override
     public String toString() {
         return "DishOverviewDto{" + "id=" + id + ", mainImage='" + mainImage + '\'' + ", alt='" + alt + '\'' + ", title='" + title + '\'' + ", description='" + description + '\'' + ", normalPrice='" + normalPrice + '\'' + ", salePrice='" + salePrice + '\'' + ", deliveryTypes=" + deliveryTypes + ", badges=" + badges + '}';
+    }
+
+    public static class Builder {
+        private final Long id;
+        private String mainImage;
+        private String alt;
+        private String title;
+        private String description;
+        private String normalPrice;
+        private String salePrice;
+
+        public Builder(Long id) {
+            this.id = id;
+        }
+
+        public Builder mainImage(String mainImage) {
+            this.mainImage = mainImage;
+            return this;
+        }
+
+        public Builder alt(String alt) {
+            this.alt = alt;
+            return this;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder normalPrice(String normalPrice) {
+            this.normalPrice = normalPrice;
+            return this;
+        }
+
+        public Builder salePrice(String salePrice) {
+            this.salePrice = salePrice;
+            return this;
+        }
+
+        public DishOverviewDto build() {
+            return new DishOverviewDto(this);
+        }
     }
 }
