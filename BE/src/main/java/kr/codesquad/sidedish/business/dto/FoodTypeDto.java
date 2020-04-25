7145bdd6fac9fe9553917f96b531b7fa95d15e38
IdionKim <ksundong@gmail.com>
2020-04-25 23:43:48 +0900

    [#69] refactor: FoodTypeDto 리팩토링
    일부 setter를 제거하고, Dto 자체를 어느정도 Immutable하게 만듭니다.
    Builder 패턴을 적용해보았습니다.

--- BE/src/main/java/kr/codesquad/sidedish/business/dto/FoodTypeDto.java
+++ BE/src/main/java/kr/codesquad/sidedish/business/dto/FoodTypeDto.java
@@ -5,33 +5,27 @@ import java.util.List;
 
 public class FoodTypeDto {
 
-    private Long id;
-    private String name;
-    private String description;
+    private final Long id;
+    private final String name;
+    private final String description;
     private List<DishOverviewDto> dishes = new ArrayList<>();
+
+    public FoodTypeDto(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public List<DishOverviewDto> getDishes() {
