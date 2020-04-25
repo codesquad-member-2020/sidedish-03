1b0d397714f80d6ae8f5dffe0daba9e9b4e96c5e
IdionKim <ksundong@gmail.com>
2020-04-25 19:02:35 +0900

    [#69] refactor: BadgeDto 리팩토링
    setter를 제거하고, Dto 자체를 Immutable하게 만듭니다.

--- BE/src/main/java/kr/codesquad/sidedish/business/dto/BadgeDto.java
+++ BE/src/main/java/kr/codesquad/sidedish/business/dto/BadgeDto.java
@@ -1,23 +1,20 @@
 package kr.codesquad.sidedish.business.dto;
 
 public class BadgeDto {
-    private String name;
-    private String color;
+    private final String name;
+    private final String color;
+
+    public BadgeDto(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getColor() {
         return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     @Override
