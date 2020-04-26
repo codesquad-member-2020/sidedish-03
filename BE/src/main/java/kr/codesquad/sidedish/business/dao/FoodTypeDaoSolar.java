de5839e0aed50ce3f68801d61337e4487b821937
blossun <dnsngu@gmail.com>
2020-04-26 23:47:01 +0900

    [#32] 모든 FoodType에 해당하는 정보 출력
    - FoodTypeController : showAllFoodTypes() 모든 FoodType에 대한 정보를 처리하는 url 추가
    - FoodTypeDao : findAllFoodTypes() 추가, 중복 메서드 referDishOverview()로 추출
    - DishOverviewDtoMapper, FoodTypeDtoMapper 클래스 추출

--- BE/src/main/java/kr/codesquad/sidedish/business/dao/FoodTypeDaoSolar.java
+++ BE/src/main/java/kr/codesquad/sidedish/business/dao/FoodTypeDaoSolar.java
@@ -1,8 +1,10 @@
 package kr.codesquad.sidedish.business.dao;
 
-import kr.codesquad.sidedish.business.dto.FoodTypeDto;
-import kr.codesquad.sidedish.business.dto.BadgeDto;
 import kr.codesquad.sidedish.business.dto.DishOverviewDto;
+import kr.codesquad.sidedish.business.dto.FoodTypeDto;
+import kr.codesquad.sidedish.business.mapper.BadgeDtoMapper;
+import kr.codesquad.sidedish.business.mapper.DishOverviewDtoMapper;
+import kr.codesquad.sidedish.business.mapper.FoodTypeDtoMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -11,51 +13,50 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
 import java.util.List;
 
 @Repository
 public class FoodTypeDaoSolar implements FoodTypeDao {
     private static final Logger log = LoggerFactory.getLogger(FoodTypeDaoSolar.class);
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplat;
+    private final FoodTypeDtoMapper foodTypeDtoMapper = new FoodTypeDtoMapper();
+    private final DishOverviewDtoMapper dishOverviewDtoMapper = new DishOverviewDtoMapper();
+    private final BadgeDtoMapper badgeDtoMapper = new BadgeDtoMapper();
 
     public FoodTypeDaoSolar(DataSource dataSource) {
         this.namedParameterJdbcTemplat = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
-    public List<FoodTypeDto> findAllFoodType() {
-        return null;
+    public List<FoodTypeDto> findAllFoodTypes() {
+        String sqlAllFoodTypes = "select id, name, description from food_type";
+        List<FoodTypeDto> foodTypeDtos = namedParameterJdbcTemplat.query(sqlAllFoodTypes, foodTypeDtoMapper);
+        for (FoodTypeDto foodTypeDto : foodTypeDtos) {
+            referDishOverview(new MapSqlParameterSource("id", foodTypeDto.getId()), foodTypeDto);
+        }
+        return foodTypeDtos;
     }
 
+    @Override
     public FoodTypeDto findDishesByFoodTypeId(Long foodTypeId) {
         SqlParameterSource parameters = new MapSqlParameterSource().addValue("id", foodTypeId);
 
         String sqlFindFoodType = "SELECT id, name, description FROM food_type WHERE id = :id";
-        FoodTypeDto foodTypeDto = namedParameterJdbcTemplat.queryForObject(sqlFindFoodType, parameters, (ResultSet rs, int rowNum) -> {
-            FoodTypeDto dto = new FoodTypeDto();
-            dto.setId(rs.getLong("id"));
-            dto.setName(rs.getString("name"));
-            dto.setDescription(rs.getString("description"));
-            return dto;
-        });
+        FoodTypeDto foodTypeDto = namedParameterJdbcTemplat.queryForObject(sqlFindFoodType, parameters, foodTypeDtoMapper);
 
-        String sqlDishesOverview = "SELECT sd.id, image.url AS main_image, sd.alt, sd.title, sd.description, sd.normal_price, sd.sale_price" +
+        referDishOverview(parameters, foodTypeDto);
+
+        return foodTypeDto;
+    }
+
+    private void referDishOverview(SqlParameterSource parameters, FoodTypeDto foodTypeDto) {
+        String sqlDishesOverview = "SELECT sd.id, image.url AS main_image, sd.alt, sd.title, sd.description" +
                 ", FORMAT(sd.normal_price, 0) AS normal_price" +
                 ", FORMAT(sd.sale_price, 0) AS sale_price" +
                 " FROM side_dish sd" +
                 " LEFT OUTER JOIN image ON sd.main_image = image.id" +
                 " WHERE sd.food_type = :id";
-        List<DishOverviewDto> dishes = namedParameterJdbcTemplat.query(sqlDishesOverview, parameters, (ResultSet rs, int rowNum) -> {
-            DishOverviewDto dto = new DishOverviewDto();
-            dto.setId(rs.getLong("id"));
-            dto.setMainImage(rs.getString("main_image"));
-            dto.setAlt(rs.getString("alt"));
-            dto.setTitle(rs.getString("title"));
-            dto.setNormalPrice(rs.getString("normal_price"));
-            dto.setSalePrice(rs.getString("sale_price"));
-            return dto;
-        });
+        List<DishOverviewDto> dishes = namedParameterJdbcTemplat.query(sqlDishesOverview, parameters, dishOverviewDtoMapper);
 
         String sqlDeliveryType = "SELECT dt.name" +
                 " FROM side_dish sd" +
@@ -70,17 +71,9 @@ public class FoodTypeDaoSolar implements FoodTypeDao {
 
         for (DishOverviewDto dish : dishes) {
             SqlParameterSource parameterDishId = new MapSqlParameterSource("id", dish.getId());
-
             dish.setDeliveryTypes(namedParameterJdbcTemplat.queryForList(sqlDeliveryType, parameterDishId, String.class));
-            dish.setBadges(namedParameterJdbcTemplat.query(sqlBadgeType, parameterDishId, (ResultSet rs, int rowNum) -> {
-                BadgeDto badgeDto = new BadgeDto();
-                badgeDto.setName(rs.getString("name"));
-                badgeDto.setColor(rs.getString("color"));
-                return badgeDto;
-            }));
+            dish.setBadges(namedParameterJdbcTemplat.query(sqlBadgeType, parameterDishId, badgeDtoMapper));
         }
         foodTypeDto.setDishes(dishes);
-
-        return foodTypeDto;
     }
 }
