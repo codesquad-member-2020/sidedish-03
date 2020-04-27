bdcef6e5da0d950437422ded8326365a74865af0
baekCode <sosaer88@gmail.com>
2020-04-28 00:50:15 +0900

    [#75] refactor: Datail component 리팩토링(컴포넌트 나눔)

--- FE/sidedish/src/components/Detail.js
+++ FE/sidedish/src/components/Detail.js
@@ -1,116 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { IoMdClose } from 'react-icons/io';
-import { BadgeBox, Mask, CloseButtn } from './styled/Common';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { Mask, CloseButtn } from './styled/Common';
+import Head from './Detail/Head';
+import Body from './Detail/Body';
 
 const Detail = props => {
   const propsData = props.data.item;
-  const setPrice = parseInt(propsData.salePrice.replace(',', ''));
-  const [quantity, setQuantity] = useState(1);
-  const [totalPrice, setTotalPrice] = useState(setPrice);
+
   const closeDetail = e => {
     const target = e.currentTarget;
     return props.onClickHandler(target);
   };
-  const onIncrease = () => {
-    setQuantity(prevQuantity => prevQuantity + 1);
-    return setTotalPrice(setPrice * (quantity + 1));
-  };
-  const onDecrease = () => {
-    if (quantity === 1) return;
-    setQuantity(prevQuantity => prevQuantity - 1);
-    return setTotalPrice(prevPrice => prevPrice - setPrice);
-  };
-  const priceCommas = price => {
-    const regComma = /\B(?=(\d{3})+(?!\d))/g;
-    return price.toString().replace(regComma, ',');
-  };
 
-  const settings = {
-    customPaging: function (i) {
-      return <img src={`${propsData.thumbImages[i]}`} alt={props.data.title} />;
-    },
-    dots: true,
-    dotsClass: 'slick-dots slick-thumb',
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
     <Mask className='mask' disappear={props.disappear}>
       <div className='detail-wrap'>
-        <div className='detail-head'>
-          <Slider {...settings} className='detail-thumb'>
-            {propsData.thumbImages.map((img, index) => (
-              <div className='thumb' key={index}>
-                <img src={img} alt={props.data.title} />
-              </div>
-            ))}
-          </Slider>
-          <div className='detail-info'>
-            <div className='badge'>
-              {propsData.badges && propsData.badges.map((badge, index) => (
-                    <BadgeBox key={index} backgroundColor={badge.color}>
-                      {badge.name}
-                    </BadgeBox>
-                  ))}
-            </div>
-            <h3 className='title'>{props.data.title}</h3>
-            <p className='description'>{propsData.description}</p>
-            <div className='table'>
-              <div>
-                <span>적립금</span>
-                <span>{propsData.point}</span>
-              </div>
-              <div>
-                <span>배송정보</span>
-                <span>{propsData.deliveryInfo}</span>
-              </div>
-              <div>
-                <span>배송비</span>
-                <span>{propsData.deliveryFee}</span>
-              </div>
-            </div>
-            <div className='price'>
-              <div className='item-price'>
-                {propsData.normalPrice === propsData.salePrice ? '' : <del>{propsData.normalPrice}</del>}
-                {propsData.salePrice} 원
-              </div>
-            </div>
-            <div className='quantity'>
-              <p className='tit'>수량 선택</p>
-              <div className='quantity-box'>
-                <button className='btn-minus' onClick={onIncrease}>
-                  +
-                </button>
-                <input type='number' className='inp-qty' value={quantity} readOnly />
-                <button className='btn-plus' onClick={onDecrease}>
-                  -
-                </button>
-              </div>
-            </div>
-            <div className='total'>
-              <p className='tit'>총 금액</p>
-              <div className='total-price'>
-                <span>{priceCommas(totalPrice)}</span>원
-              </div>
-            </div>
-            <div className='btn-wrap'>
-              <button>담기</button>
-            </div>
-          </div>
-        </div>
-        <div className='detail-body'>
-          <h3>상세정보</h3>
-          {propsData.detailSectionImages.map((item, index) => (
-            <p key={index}>
-              <img src={item} alt={props.data.title} />
-            </p>
-          ))}
-        </div>
+        <Head detailTitle={props.data.title} detailData={propsData} />
+        <Body detaileImages={propsData.detailSectionImages} imgAlt={props.data.title} />
       </div>
       <CloseButtn className='btn-close' onClick={closeDetail}>
         <IoMdClose size='50' color='#bfbfbf' />
