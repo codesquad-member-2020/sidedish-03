6f3aff82fa966bb2bfc0e2a9c32525e3140dbaf4
baekCode <sosaer88@gmail.com>
2020-04-28 16:47:14 +0900

    [#75] feat: footer 임시 구현

--- FE/sidedish/src/components/Footer.js
+++ FE/sidedish/src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const FooterStyled = styled.div`
+  padding: 15px 0;
+  border-top: 1px solid #e9e9e9;
+  background: #fafafa;
+`;
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <FooterStyled className='footer'>
+      <div className='inner'>Team#3 Project</div>
+    </FooterStyled>
+  );
 };
 
 export default Footer;
