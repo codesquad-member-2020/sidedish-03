475757dc360633f6214f736927297ddfbbe9d0ce
blossun <dnsngu@gmail.com>
2020-04-28 14:39:55 +0900

    [#37] 환경변수 설정 추가
    - 기존 데이터 삭제 순서 변경

--- BE/build.sh
+++ BE/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "===== Check update =====>"
 
+export JASYPT_PASSWORD="sidedish";
+export GITHUB_CLIENT_ID="bed01aae4e0ea3bebf24";
+export GITHUB_CLIENT_SECRET="a6a4a7e18bbddd855c868c2bdf0d280219d42e35";
+
 # check update
 cd sidedish-03/BE
 git fetch
@@ -14,23 +18,23 @@ if [ $now == $origin ]; then
         echo "Already up to date"
 else
         # shutdown server
-        ~/apache-tomcat-9.0.34/bin/shutdown.sh
-
-        # remove old data
-        rm -rf ~/apache-tomcat-9.0.34/webapps/ROOT
-        rm -rf ~/apache-tomcat-9.0.34/webapps/ROOT.war
+        /home/ubuntu/apache-tomcat-9.0.34/bin/shutdown.sh
 
         # merge and build
         echo "***** BUILD START *****"
         git pull
         ./gradlew build -x test
 
+        # remove old data
+        rm -rf /home/ubuntu/apache-tomcat-9.0.34/webapps/ROOT
+        rm -rf /home/ubuntu/apache-tomcat-9.0.34/webapps/ROOT.war
+
         # move to WEBROOT
-        cp ./build/libs/ROOT.war ~/apache-tomcat-9.0.34/webapps/
+        cp ./build/libs/ROOT.war /home/ubuntu/apache-tomcat-9.0.34/webapps/
 
         # restart tomcat
-        ~/apache-tomcat-9.0.34/bin/startup.sh
+        sleep 5
+        /home/ubuntu/apache-tomcat-9.0.34/bin/startup.sh
 
         echo "***** SERVER RESTARTED ***** "
 fi
-echo "<====== End Update ====="
