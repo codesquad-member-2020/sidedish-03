9dad982eb2e9004b4f671d7397022750247aaf31
blossun <dnsngu@gmail.com>
2020-04-28 18:32:15 +0900

    [#86] 로그인후 메인페이지로 redirect

--- BE/src/main/java/kr/codesquad/sidedish/common/login/LoginController.java
+++ BE/src/main/java/kr/codesquad/sidedish/common/login/LoginController.java
@@ -1,13 +1,19 @@
 package kr.codesquad.sidedish.common.login;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
+import java.io.IOException;
 
 @RestController
 public class LoginController {
@@ -19,11 +25,26 @@ public class LoginController {
     }
 
     @GetMapping("/githublogin")
-    public ResponseEntity<String> githubLogin(@PathParam("code") String code, HttpServletResponse response) {
+    public ResponseEntity<String> githubLogin(@PathParam("code") String code, HttpServletResponse response) throws IOException {
         log.debug("code : {}", code);
         GithubToken githubToken = loginService.getAccessToken(code);
-        response.setHeader("Authorization", githubToken.getAuthorizationValue());
-        //TODO : 응답객체를 만들어서 Wrapping하여 반환
-        return ResponseEntity.ok("logined");
+        loginService.getUserEmail(githubToken.getAuthorizationValue());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userInfo = loginService.getUserEmail(githubToken.getAuthorizationValue());
+        JsonNode user = objectMapper.readTree(userInfo);
+        for (JsonNode jsonNode : user) {
+            if (jsonNode.get("primary").asBoolean()) {
+                response.addCookie(new Cookie("email", jsonNode.get("email").textValue()));
+            }
+        }
+
+        log.debug("userEmail : {}", user.get("email"));
+        String redirectUrl = "http://15.165.21.99";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", redirectUrl);
+        HttpStatus status = HttpStatus.TEMPORARY_REDIRECT;
+        response.sendRedirect(redirectUrl);
+        return new ResponseEntity<>(headers, status);
     }
 }
--- BE/src/main/java/kr/codesquad/sidedish/common/login/LoginService.java
+++ BE/src/main/java/kr/codesquad/sidedish/common/login/LoginService.java
@@ -3,6 +3,8 @@ package kr.codesquad.sidedish.common.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -15,7 +17,8 @@ import java.util.Map;
 @Service
 public class LoginService {
     private static final Logger log = LoggerFactory.getLogger(LoginService.class);
-    private String url = "https://github.com/login/oauth/access_token";
+    private String getAccessTokenUrl = "https://github.com/login/oauth/access_token";
+    private String getUserEmailUrl = "https://api.github.com/user/emails";
     private String clientId = System.getenv("GITHUB_CLIENT_ID");
     private String clientSecret = System.getenv("GITHUB_CLIENT_SECRET");
 
@@ -25,6 +28,7 @@ public class LoginService {
         header.put("Accept", "application/json");
         headers.setAll(header);
 
+        log.debug("client_secret : {}", clientSecret);
         MultiValueMap<String, String> requestPayloads = new LinkedMultiValueMap<>();
         Map<String, String> requestPayload = new HashMap<>();
         requestPayload.put("client_id", clientId);
@@ -33,8 +37,18 @@ public class LoginService {
         requestPayloads.setAll(requestPayload);
 
         HttpEntity<?> request = new HttpEntity<>(requestPayloads, headers);
-        ResponseEntity<?> response = new RestTemplate().postForEntity(url, request, GithubToken.class);
+        ResponseEntity<?> response = new RestTemplate().postForEntity(getAccessTokenUrl, request, GithubToken.class);
         return (GithubToken) response.getBody();
+    }
+
+    public String getUserEmail(String githubToken) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", githubToken);
+
+        ResponseEntity<String> response = restTemplate.exchange(getUserEmailUrl, HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+        log.debug("getUserEmail Response : {}", response.getBody());
+        return response.getBody();
     }
 
 }
