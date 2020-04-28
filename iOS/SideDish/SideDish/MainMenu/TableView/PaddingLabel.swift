6fd45b87b14e3e8426ec0db6563efd47a15eaba8
GangWoon <gangwoon2018@gmail.com>
2020-04-28 13:36:49 +0900

    [#77] feat: PaddingLabel 기능 구현
    init(coder:)가 불려도 동일한 작업을 할 수 있도록 설계해야된다고 말씀하셨던게 기억이 납니다. 그러면 의미에 기본값을 주더라도 동작하도록 만들어야 하는지 아니면 정말로 init(insets:)과 동일하게 작동하도록 만들어야 하는지 궁금합니다.

--- iOS/SideDish/SideDish/MainMenu/TableView/PaddingLabel.swift
+++ iOS/SideDish/SideDish/MainMenu/TableView/PaddingLabel.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2020 Cloud. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+
+class PaddingLabel: UILabel {
+    
+    // MARK: - Properties
+    typealias Inset = (top: CGFloat, left: CGFloat, right: CGFloat, bottom: CGFloat)
+    static let inset: Inset = Inset(1, 2, 2, 1)
+    var topInset: CGFloat!
+    var leftInset: CGFloat!
+    var rightInset: CGFloat!
+    var bottomInset: CGFloat!
+    override var intrinsicContentSize: CGSize {
+        get {
+            var contentSize = super.intrinsicContentSize
+            contentSize.height += topInset + bottomInset
+            contentSize.width += leftInset + rightInset
+            return contentSize
+        }
+    }
+
+    // MARK: - Lifecycle
+    required init(insets: Inset = Inset(0, 0, 0, 0)) {
+        super.init(frame: CGRect.zero)
+        topInset = insets.top
+        leftInset = insets.left
+        rightInset = insets.right
+        bottomInset = insets.bottom
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        basicConfigureInsets()
+    }
+    
+    // MARK: - Methods
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    private func basicConfigureInsets() {
+        topInset = 0
+        leftInset = 0
+        rightInset = 0
+        bottomInset = 0
+    }
+}
+
+
+
