6dd100d6d2ed9efd52e7eed6fb10a9432af45f6c
blossun <dnsngu@gmail.com>
2020-04-29 12:06:31 +0900

    [#86] refactor: 하드코딩 내용 삭제
    userEmail정보를 파싱하는 부분을 loginService.getUserEmail에서 처리하도록 수정

--- BE/src/main/java/kr/codesquad/sidedish/common/login/LoginController.java
+++ BE/src/main/java/kr/codesquad/sidedish/common/login/LoginController.java
@@ -1,16 +1,14 @@
 package kr.codesquad.sidedish.common.login;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 import java.io.IOException;
@@ -25,26 +23,14 @@ public class LoginController {
     }
 
     @GetMapping("/githublogin")
-    public ResponseEntity<String> githubLogin(@PathParam("code") String code, HttpServletResponse response) throws IOException {
+    public ResponseEntity<String> githubLogin(@PathParam("code") String code, HttpServletRequest request, HttpServletResponse response) throws IOException {
         log.debug("code : {}", code);
         GithubToken githubToken = loginService.getAccessToken(code);
-        loginService.getUserEmail(githubToken.getAuthorizationValue());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String userInfo = loginService.getUserEmail(githubToken.getAuthorizationValue());
-        JsonNode user = objectMapper.readTree(userInfo);
-        for (JsonNode jsonNode : user) {
-            if (jsonNode.get("primary").asBoolean()) {
-                response.addCookie(new Cookie("email", jsonNode.get("email").textValue()));
-            }
-        }
-
-        log.debug("userEmail : {}", user.get("email"));
-        String redirectUrl = "http://15.165.21.99";
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", redirectUrl);
-        HttpStatus status = HttpStatus.TEMPORARY_REDIRECT;
+        String userEmail = loginService.getUserEmail(githubToken.getAuthorizationValue());
+        response.addCookie(new Cookie("email", userEmail));
+        log.debug("userEmail : {}", userEmail);
+        String redirectUrl = request.getHeader("referer");
         response.sendRedirect(redirectUrl);
-        return new ResponseEntity<>(headers, status);
+        return new ResponseEntity<>(HttpStatus.TEMPORARY_REDIRECT);
     }
 }
